f31a55df2e346e76b406274b3056390ef235b359
Madelyn Romero <m.romero013@gmail.com>
2020-02-19 20:32:14 -0700

    Add requires for sinatra, active record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
-class ApplicationController < Sinatra::Base 
+require "sinatra"
+require 'sinatra/activerecord'
+
+class ApplicationController < Sinatra::Base
   configure do
     set :view, "app/views"
   end
   get '/' do
     "This is an example route"
   end
-end 
+end
