a5d71f29d2d9409f1d02c3170de155b69524cf5f
Ryan Allen <rallenwasd@gmail.com>
2020-02-20 17:44:37 -0700

    Add retrieve_location spike

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 require "sinatra"
 require 'sinatra/activerecord'
+require 'rubygems'
+require 'httparty'
 
 class ApplicationController < Sinatra::Base
-  configure do
-    set :view, "app/views"
-  end
   get '/' do
-    "This is an example route"
+    "Root page of Sinatra App - Microservice API"
+  end
+
+  get '/places/:destination' do
+    retrieve_location(params["destination"])
+  end
+
+  def retrieve_location(destination)
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{destination}&inputtype=textquery&fields=geometry&key=#{ENV['GOOGLE_API_KEY']}")
+    coords = response["candidates"].first["geometry"]["location"]
+    #  # mocked coords
+    # coords = {"lat"=>39.7392358, "lng"=>-104.990251} # denver
+    # coords = {"lat"=>29.7604267, "lng"=>-95.3698028} # houston
+    require "pry"; binding.pry
   end
 end
