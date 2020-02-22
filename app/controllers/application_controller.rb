7d890a350e28e12719a62e9e155e83c53a9273e8
Ryan Allen <rallenwasd@gmail.com>
2020-02-22 11:47:27 -0700

    Add google places spike

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/places/:destination' do
-    retrieve_location(params["destination"])
+    # checks to see if the location is in the db
+    # if not do below
+    (params["destination"])
+    # create new instance of serializer
   end
 
-  def retrieve_location(destination)
+  def retrieve_coords(destination)
     response = HTTParty.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{destination}&inputtype=textquery&fields=geometry&key=#{ENV['GOOGLE_API_KEY']}")
     coords = response["candidates"].first["geometry"]["location"]
     #  # mocked coords
     # coords = {"lat"=>39.7392358, "lng"=>-104.990251} # denver
     # coords = {"lat"=>29.7604267, "lng"=>-95.3698028} # houston
-    require "pry"; binding.pry
   end
 end
