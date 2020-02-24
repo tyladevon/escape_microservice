cfbf0179aa0bf56acd468225d13bb65e8203a93f
Madelyn Romero <m.romero013@gmail.com>
2020-02-24 12:06:02 -0700

    Create test for Mountain Project API call

---
+++ spec/requests/v1/mountain_project_endpoint_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Mountain Project API endpoint" do
+  include Rack::Test::Methods
+
+  def app
+    ApplicationController
+  end
+
+  it "can retrieve climb routes with given query params", :vcr do
+    lat_param = 40.03
+    lon_param = -105.25
+    max_dist_param = 10
+    min_diff_param = 5.7
+    max_diff_param = 5.12
+
+    get "/api/v1/climb_results",
+    params={lat: lat_param,
+            lon: lon_param,
+            max_dist: max_dist_param,
+            min_diff: min_diff_param,
+            max_diff: max_diff_param}
+    expect(last_response).to be_successful
+    parsed_routes = JSON.parse(last_response.body)["routes"]
+    expect(parsed_routes.count).to eq(2)
+
+
+  end
+
+end
