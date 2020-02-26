c65a6d458be74501ccf0ac16fc4429e008df0420
Alison Vermeil <alison.minton@gmail.com>
2020-02-25 20:55:42 -0700

    Refactor hikes controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,11 +1,9 @@
+require './app/services/hiking_project_service'
+require './app/serializers/hikes_serializer'
 
 class HikesController
-  def self.search(incoming_params)
-    lat = incoming_params[:lat]
-    lon = incoming_params[:lon]
-    max_dist = incoming_params[:max_dist]
-    HTTParty.get("https://www.hikingproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{max_dist}&key=#{ENV['HIKE_API_KEY']}")
-    # HTTParty.get("https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=#{ENV['HIKE_API_KEY']}")
-    # response = HTTParty.get("https://www.mountainproject.com/data/get-routes-for-lat-lon?key=#{ENV['MOUNTAIN_API_KEY']}&lat=#{incoming_params[:lat]}&lon=#{incoming_params[:lon]}&maxDistance=#{incoming_params[:maxDist]}&maxResults=2&minDiff=#{incoming_params[:min_diff]}&maxDiff=#{incoming_params[:max_diff]}")
+  def self.search(climb_params)
+    hikes = HikingProjectService.search_hikes(climb_params)
+    HikesSerializer.new(hikes).present
   end
 end
