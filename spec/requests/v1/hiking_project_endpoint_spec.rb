3bf68d0f7e4eb892b09e223f25543b5d985a91fc
Alison Vermeil <alison.minton@gmail.com>
2020-02-25 19:03:54 -0700

    Fix expectation

--- spec/requests/v1/hiking_project_endpoint_spec.rb
+++ spec/requests/v1/hiking_project_endpoint_spec.rb
@@ -16,20 +16,21 @@ describe "Hiking Project API endpoint" do
 
 
     expect(last_response).to be_successful
-    parsed_hikes = JSON.parse(last_response.body)["routes"]
-    # expect(parsed_hikes.count).to eq(0)
+    parsed_hikes = JSON.parse(last_response.body)
+    expect(parsed_hikes['trails'].count).to eq(10)
+
     expect(parsed_hikes).to have_key('trails')
-    expect(parsed_hikes.first).to have_key('name')
-    expect(parsed_hikes.first).to have_key('type')
-    expect(parsed_hikes.first).to have_key('summary')
-    expect(parsed_hikes.first).to have_key('difficulty')
-    expect(parsed_hikes.first).to have_key('stars')
-    expect(parsed_hikes.first).to have_key('location')
-    expect(parsed_hikes.first).to have_key('length')
-    expect(parsed_hikes.first).to have_key('ascent')
-    expect(parsed_hikes.first).to have_key('descent')
-    expect(parsed_hikes.first).to have_key('longitude')
-    expect(parsed_hikes.first).to have_key('latitude')
+    expect(parsed_hikes['trails'].first).to have_key('name')
+    expect(parsed_hikes['trails'].first).to have_key('type')
+    expect(parsed_hikes['trails'].first).to have_key('summary')
+    expect(parsed_hikes['trails'].first).to have_key('difficulty')
+    expect(parsed_hikes['trails'].first).to have_key('stars')
+    expect(parsed_hikes['trails'].first).to have_key('location')
+    expect(parsed_hikes['trails'].first).to have_key('length')
+    expect(parsed_hikes['trails'].first).to have_key('ascent')
+    expect(parsed_hikes['trails'].first).to have_key('descent')
+    expect(parsed_hikes['trails'].first).to have_key('longitude')
+    expect(parsed_hikes['trails'].first).to have_key('latitude')
 
   end
 end
